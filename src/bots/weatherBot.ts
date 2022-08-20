acae69ef9c3500c9b6a66d008cd82617dfcedf75
Armand <armandbernard1995@gmail.com>
2022-08-20 21:28:11 +0100

    create a weather bot

---
+++ src/bots/weatherBot.ts
@@ -0,0 +1,87 @@
+import { GatewayIntentBits, IntentsBitField } from "discord.js";
+import { ILogger } from "../ILogger";
+import { BotBase } from "./botBase";
+import fetch from "cross-fetch";
+
+const intents: GatewayIntentBits[] = [
+  IntentsBitField.Flags.Guilds,
+  IntentsBitField.Flags.GuildMessages,
+  IntentsBitField.Flags.MessageContent,
+];
+
+const regex = /\bweather\b/i;
+
+const apiUrl = "https://api.weatherapi.com/v1";
+
+interface WeatherBotProps {
+  /**
+   * The token the bot will use to log in
+   */
+  token: string;
+  weatherKey: string;
+  /**
+   * The logger the bot will use to log messages
+   */
+  logger: ILogger;
+}
+
+export class WeatherBot extends BotBase {
+  weatherKey: string;
+
+  constructor(props: WeatherBotProps) {
+    super({
+      ...props,
+      name: "weatherBot",
+      intents,
+    });
+
+    this.weatherKey = props.weatherKey;
+
+    // when a message is created
+    this.client.on("messageCreate", async (message) => {
+      if (!WeatherBot.containsWeather(message.content)) {
+        return;
+      }
+
+      const url = `${apiUrl}/current.json?key=${this.weatherKey}&q=London`;
+
+      const response = await fetch(url);
+
+      const result = (await response.json()) as APIResponse;
+
+      const name = result.location.name;
+      const temp = result.current.temp_c;
+
+      const messageToSend = `In ${name}, the temperature is ${temp}°C`;
+
+      message.channel.send(messageToSend);
+    });
+
+    this.login();
+  }
+
+  /**
+   * Check if the message contains "weather"
+   * @param message the message to check
+   * @returns true if the message contains "weather"
+   */
+  static containsWeather(message: string): boolean {
+    return regex.exec(message) != null;
+  }
+}
+
+interface APIResponse {
+  location: APILocation;
+  current: CurrentWeather;
+}
+
+interface APILocation {
+  name: string;
+  region: string;
+  country: string;
+  localTime: string;
+}
+
+interface CurrentWeather {
+  temp_c: number;
+}
