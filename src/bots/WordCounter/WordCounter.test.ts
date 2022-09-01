f4eacfe8c9db89408b243a8516df5b74686f7591
Armand <armandbernard1995@gmail.com>
2022-09-01 08:53:06 +0100

    Add some tests to define expected object parse behaviour

--- src/bots/WordCounter/WordCounter.test.ts
+++ src/bots/WordCounter/WordCounter.test.ts
@@ -1,3 +1,4 @@
+import { Message } from "discord.js";
 import { WordCounter } from "./WordCounter";
 
 const expectedParsedWords = [
@@ -5,6 +6,18 @@ const expectedParsedWords = [
   [" word ", "word"],
   [" multiple words ", "multiple words"],
   ["HQW£%U£J$%^J", "HQW£%U£J$%^J"],
+];
+
+const makeMsg = (content: string): Message => {
+  return { content } as Message;
+};
+
+const messageParserCases: [string, string, boolean, string][] = [
+  ["Hi, how are you?", "are", true, "it's a full word"],
+  ["Hi, how are you?", "Hi,", true, "it includes a comma"],
+  ["Hi, how are you?", "ou", false, "it's not a complete word"],
+  ["Hi, how are you?", "HOW", true, "it should not be case sensitive"],
+  ["Hi, how are you?", "how are", true, "it should handle multiple words"],
 ];
 
 describe("WordCounter", () => {
@@ -36,5 +49,15 @@ describe("WordCounter", () => {
         WordCounter.parseRequest("I think <@&1234> is better than <@&5678>")
       ).toBe("I think  is better than");
     });
+  });
+  describe("message parser", () => {
+    it.each(messageParserCases)(
+      "When given '%s' and looking for '%s', it returns '%s' because %s.",
+      (message, search, expectedResult) => {
+        expect(WordCounter.checkMessage(makeMsg(message), search)).toBe(
+          expectedResult
+        );
+      }
+    );
   });
 });
