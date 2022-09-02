03ae7a5c90451fb325e8b056f2ae9bd376472db0
Armand <armandbernard1995@gmail.com>
2022-09-02 08:41:19 +0100

    remove associated tests

--- src/bots/WordCounter/WordCounter.test.ts
+++ src/bots/WordCounter/WordCounter.test.ts
@@ -27,23 +27,6 @@ const messageParserCases: [string, string, boolean, string][] = [
 ];
 
 describe("WordCounter", () => {
-  describe("regex for finding mentions", () => {
-    it("finds a mention at the start", () => {
-      const message = "<@&1014610397430550604> how are you doing?";
-
-      expect(WordCounter.mentionsRegex.test(message)).toBe(true);
-    });
-    it("does not find a mention where there is none", () => {
-      const message = "Totally normal message";
-
-      expect(WordCounter.mentionsRegex.test(message)).toBe(false);
-    });
-    it("finds a mention even when it is surrounded by text", () => {
-      const message = "I think <@&1014610397430550604>'s code is broken";
-
-      expect(WordCounter.mentionsRegex.test(message)).toBe(true);
-    });
-  });
   describe("request parser", () => {
     describe("correctly pulls intended words", () => {
       it.each(expectedParsedWords)("takes '%s' and returns '%s'", (a, b) => {
