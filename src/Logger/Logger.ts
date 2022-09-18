67e24425b551c3036ebc8bf05e2690473e96fe52
Armand <armandbernard1995@gmail.com>
2022-09-18 08:58:10 +0100

    improve the regular logger

--- src/Logger/Logger.ts
+++ src/Logger/Logger.ts
@@ -2,12 +2,16 @@ import { ILogger } from "./ILogger";
 
 export class Logger implements ILogger {
   log(message: string) {
-    console.log(message);
+    console.log(this.constructString("INFO", message));
   }
   warn(message: string): void {
-    console.warn(message);
+    console.warn(this.constructString("WARN", message));
   }
   error(message: string) {
-    console.error(message);
+    console.error(this.constructString("ERROR", message));
+  }
+
+  constructString(level: "INFO" | "WARN" | "ERROR", message: string) {
+    return `[${level}][${new Date().toISOString()}] ${message}`;
   }
 }
